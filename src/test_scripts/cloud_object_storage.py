f50e8e99b22354e2a3ffd4f99f4a03bb4bb9a357
Felix Chen <felix.chen@ibm.com>
2020-05-03 10:55:16 -0400

    screenshot application kubernetes service and deployment

--- src/test_scripts/cloud_object_storage.py
+++ src/test_scripts/cloud_object_storage.py
@@ -4,9 +4,8 @@ url = 'http://0.0.0.0:8001'
 files = {'file': open('100303030222.png', 'rb')}
 
 
-# r = requests.post(f'{url}/set', files=files)
-# print(r.status_code)
-
+r = requests.post(f'{url}/set', files=files)
+print(r.status_code)
 
 payload = {
     'file_ID':  '100303030222.png'
