d99f8e5f38b6ddc1546736f443c677f95494c9a1
Felix Chen <felix.chen@ibm.com>
2020-05-09 21:46:35 -0400

    adding a new watcher now takes a screenshot and uploads it to COS

--- src/configure/app.py
+++ src/configure/app.py
@@ -1,7 +1,29 @@
+import os
+import uuid
+import requests
 from flask import Flask, jsonify, request
 from configure import add_user, list_users, get_user, add_watcher, list_watchers, get_watcher
 
 app = Flask(__name__)
+
+production = 'KUBERNETES_SERVICE_HOST' in os.environ
+
+if production:
+    def get_address(host, port):
+        return f'http://{host}:{port}'
+
+    cloud_object_storage_service_host = os.environ['CLOUD_OBJECT_STORAGE_SERVICE_HOST']
+    cloud_object_storage_service_port = os.environ['CLOUD_OBJECT_STORAGE_SERVICE_PORT']
+    cloud_object_storage_service_address = get_address(
+        cloud_object_storage_service_host, cloud_object_storage_service_port)
+
+    screenshot_host = os.environ['SCREENSHOT_SERVICE_HOST']
+    screenshot_port = os.environ['SCREENSHOT_SERVICE_PORT']
+    screenshot_address = get_address(
+        screenshot_host, screenshot_port)
+else:
+    cloud_object_storage_service_address = 'http://0.0.0.0:8001'
+    screenshot_address = 'http://0.0.0.0:8003'
 
 
 @app.route('/users', methods=['GET', 'POST'])
@@ -41,7 +63,15 @@ def watchers():
 
     watcher_id = add_watcher(user_id, url, frequency)
 
-    # TODO NEED TO DO FIRST TIME SCREENSHOT
+    # FIRST TIME SCREENSHOT
+    server_file_path = os.path.join('files', f'{watcher_id}.png')
+    r = requests.get(f'{screenshot_address}/screenshot', json=data)
+    open(server_file_path, 'wb').write(r.content)
+    # Upload to COS
+    files = {'file': open(server_file_path, 'rb')}
+    r = requests.post(
+        f'{cloud_object_storage_service_address}/file', files=files)
+    os.remove(server_file_path)
 
     return f'Succesfully created watcher {watcher_id}'
 
