072b642c1dfc00e494afd5c9f6fed391fd18d85f
Felix Chen <felix.chen@ibm.com>
2020-05-11 10:12:00 -0400

    Update src/compare/compare.py

--- src/compare/compare.py
+++ src/compare/compare.py
@@ -8,6 +8,11 @@ def get_difference(file_path_old, file_path_new):
     before = cv2.imread(file_path_old)
     after = cv2.imread(file_path_new)
 
+    
+    print('Old image shape: ',type(before.shape))
+    
+    print('New image shape: ',type(after.shape))
+
     # Convert images to grayscale
     before_gray = cv2.cvtColor(before, cv2.COLOR_BGR2GRAY)
     after_gray = cv2.cvtColor(after, cv2.COLOR_BGR2GRAY)
