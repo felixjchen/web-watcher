caa2443daaafd6377291572bb5d89b3e5d08f9ff
Felix Chen <felix.chen@ibm.com>
2020-05-10 22:09:13 -0400

    finished check script

--- src/check/check.py
+++ src/check/check.py
@@ -1,34 +1,95 @@
 import os
 import requests
+import time
+import threading
+from threading import Thread
+from helpers import take_new_screenshot, get_old_screenshot, update_old_screenshot, notify, get_difference, update_last_run, cleanup
+
+# Difference tolerence
+threshold = 0
+now = int(time.time())
 
 
 production = 'KUBERNETES_SERVICE_HOST' in os.environ
 
-
-def get_address(host, port):
-    return f'http://{host}:{port}'
-
-
 if production:
-    cloud_object_storage_service_host = os.environ['CLOUD_OBJECT_STORAGE_SERVICE_HOST']
-    cloud_object_storage_service_port = os.environ['CLOUD_OBJECT_STORAGE_SERVICE_PORT']
-    cloud_object_storage_service_address = get_address(
-        cloud_object_storage_service_host, cloud_object_storage_service_port)
-
-    compare_service_host = os.environ['COMPARE_SERVICE_HOST']
-    compare_service_port = os.environ['COMPARE_SERVICE_PORT']
-    compare_service_address = get_address(
-        compare_service_host, compare_service_port)
-
-    screenshot_host = os.environ['SCREENSHOT_SERVICE_HOST']
-    screenshot_port = os.environ['SCREENSHOT_SERVICE_PORT']
-    screenshot_address = get_address(
-        screenshot_host, screenshot_port)
+    configure_host = os.environ['CONFIGURE_SERVICE_HOST']
+    configure_port = os.environ['CONFIGURE_SERVICE_PORT']
+    configuration_address = f'http://{configure_host}:{configure_port}'
 else:
-    cloud_object_storage_service_address = 'http://0.0.0.0:8001'
-    compare_service_address = 'http://0.0.0.0:8002'
-    screenshot_address = 'http://0.0.0.0:8003'
+    configure_address = 'http://0.0.0.0:8004'
+
+def run_watcher(watcher_id, data):
+    user_id = data['user_id']
+    last_run = data['last_run']
+    frequency = data['frequency']
+    url = data['url']
+
+    seconds_since_last_run = now - last_run
+    if seconds_since_last_run > frequency:
+        start = time.perf_counter()
+
+        print(f'{watcher_id}: Updating')
+
+        # Concurrently grab new and old screenshots
+        server_file_paths = {
+            'old' : None,
+            'new' : None
+        }
+        # Take new screenshot
+        t1 = Thread(target = take_new_screenshot, args = (watcher_id, url, server_file_paths, ))
+        # Get old screenshot
+        t2 = Thread(target = get_old_screenshot, args = (watcher_id, server_file_paths, ))
+        t1.start()
+        t2.start()
+        t1.join()
+        t2.join()
+
+        # Get difference and notify if appropriate
+        difference = get_difference(server_file_paths)
+
+        if difference > threshold:
+            print(f'{watcher_id}: Difference detected')
+
+            # Notify
+            t = Thread(target=notify, args=(user_id, url, ))
+            t.start()
+            
+            # Update COS image
+            t = Thread(target=update_old_screenshot, args=(server_file_paths['new'],))
+            t.start()
+
+
+        # Update last_run time  
+        t = Thread(target=update_last_run, args=(watcher_id, now,))
+        t.start()
+        # Cleanup
+        t = Thread(target=cleanup, args=(server_file_paths,))
+        t.start()
+
+        print(f'{watcher_id}: Finished in {time.perf_counter() - start} seconds')
+        
+    else:
+        print(f'{watcher_id}: {seconds_since_last_run} < {frequency}')
+
 
 
 if __name__ == "__main__":
-    pass
+
+    print('-----------------------------------------------------------')
+    print(f'Epoch Time: {now}, Main thread: {threading.get_ident()}')
+    print('-----------------------------------------------------------')
+
+    watchers = requests.get(f'{configure_address}/watchers').json()
+
+    threads = []
+    for watcher_id, data in watchers.items():
+        t = Thread(target=run_watcher, args=(watcher_id, data, ))
+        threads.append((watcher_id, t))
+        t.start()
+
+
+    for (_,thread) in threads:
+        thread.join()
+
+    print("Main thread finished")
---
+++ src/check/helpers.py
@@ -0,0 +1,99 @@
+import requests
+import os
+import threading
+
+production = 'KUBERNETES_SERVICE_HOST' in os.environ
+
+if production:
+    def get_address(host, port):
+        return f'http://{host}:{port}'
+    cloud_object_storage_service_host = os.environ['CLOUD_OBJECT_STORAGE_SERVICE_HOST']
+    cloud_object_storage_service_port = os.environ['CLOUD_OBJECT_STORAGE_SERVICE_PORT']
+    cloud_object_storage_service_address = get_address(
+        cloud_object_storage_service_host, cloud_object_storage_service_port)
+
+    compare_service_host = os.environ['COMPARE_SERVICE_HOST']
+    compare_service_port = os.environ['COMPARE_SERVICE_PORT']
+    compare_service_address = get_address(
+        compare_service_host, compare_service_port)
+
+    screenshot_host = os.environ['SCREENSHOT_SERVICE_HOST']
+    screenshot_port = os.environ['SCREENSHOT_SERVICE_PORT']
+    screenshot_address = get_address(
+        screenshot_host, screenshot_port)
+
+    
+    configure_host = os.environ['CONFIGURE_SERVICE_HOST']
+    configure_port = os.environ['CONFIGURE_SERVICE_PORT']
+    configure_address = get_address(
+        configure_host, configure_port)
+else:
+    cloud_object_storage_service_address = 'http://0.0.0.0:8001'
+    compare_service_address = 'http://0.0.0.0:8002'
+    screenshot_address = 'http://0.0.0.0:8003'
+    configure_address = 'http://0.0.0.0:8004'
+    from secrets import credentials
+
+def take_new_screenshot(watcher_id, url, server_file_paths):
+    # print(f'{watcher_id}: New screenshot thread {threading.get_ident()}')
+    server_file_path_new_screenshot = os.path.join('files', f'{watcher_id}.png')
+    server_file_path_new_screenshot = os.path.abspath(server_file_path_new_screenshot)
+    with requests.get(f'{screenshot_address}/screenshot', json={'url':url}) as response:
+        with open(server_file_path_new_screenshot, 'wb') as file:
+            file.write(response.content)
+
+    server_file_paths['new'] = server_file_path_new_screenshot
+
+
+
+def get_old_screenshot(watcher_id, server_file_paths):
+    # print(f'{watcher_id}: Old screenshot thread {threading.get_ident()}')
+    server_file_path_old_screenshot = os.path.join('files', f'{watcher_id}_old.png')
+    server_file_path_old_screenshot = os.path.abspath(server_file_path_old_screenshot)
+    with requests.get(f'{cloud_object_storage_service_address}/file/{watcher_id}.png') as response:
+        with open(server_file_path_old_screenshot, 'wb') as file:
+            file.write(response.content)
+
+    server_file_paths['old'] = server_file_path_old_screenshot
+
+
+def update_old_screenshot(server_file_path):
+    files = {'file': open(server_file_path, 'rb')}
+    requests.post(f'{cloud_object_storage_service_address}/file', files=files) 
+
+def get_difference(server_file_paths):
+    files = {
+        'file_old': open(server_file_paths['old'], 'rb'),
+        'file_new': open(server_file_paths['new'], 'rb')
+    }
+    with requests.get(f'{compare_service_address}/difference' , files=files) as response:
+        difference = float(response.text)
+    
+    return difference
+
+def notify(user_id, url):
+    # print(f'NOTIFYING USER {user_id}')
+    with requests.get(f'{configure_address}/users/{user_id}') as response:
+        data = response.json()
+
+        email = data['email']
+
+        payload = {
+            'alertType': 'email',
+            'apikey': credentials['apikey'],
+            'address': email,
+            'subject': 'web-watcher notification',
+            'body': f'{url} has changed'
+        }
+        response = requests.post("https://us-east.functions.cloud.ibm.com/api/v1/web/f20b7584-b5de-4512-b3fa-904a2ca64acd/default/alerter", data=payload)
+
+
+def update_last_run(watcher_id, now):
+    payload = {
+        'last_run': now
+    }
+    requests.put(f'{configure_address}/watchers/{watcher_id}', json = payload)
+
+def cleanup(server_file_paths):
+    os.remove(server_file_paths['new'])
+    os.remove(server_file_paths['old'])
