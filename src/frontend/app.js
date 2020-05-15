0a0fc810db9208cafff1e2131e90559d52f62dbb
Felix Chen <felix.chen@ibm.com>
2020-05-15 14:44:31 -0400

    added forms

--- src/frontend/app.js
+++ src/frontend/app.js
@@ -5,29 +5,34 @@ const app = express()
 
 const production = (typeof process.env.KUBERNETES_SERVICE_HOST !== 'undefined');
 const configure_adddress = 'http://0.0.0.0:8004';
-if (production){
+if (production) {
     configure_adddress = 'http://' + process.env.CONFIGURE_SERVICE_HOST + ':' + process.env.CONFIGURE_SERVICE_PORT
 }
 
-console.log('Configure service address at : ' + configure_adddress)
 
 nunjucks.configure('views', {
     autoescape: true,
-    express   : app
+    express: app
 });
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
 
-    function getUsers(){
+    function getUsers() {
         return axios.get(configure_adddress + '/users');
     }
-    function getWatchers(){
+
+    function getWatchers() {
         return axios.get(configure_adddress + '/watchers');
     }
 
     axios.all([getUsers(), getWatchers()]).then(axios.spread(function (users, watchers) {
-        // console.log(users.data)
-        // console.log(watchers.data)
+        console.log(watchers.data)
+        // Convert epoch to date
+        for (watcher_id in watchers.data) {
+            epoch = watchers.data[watcher_id].last_run
+            // console.log(new Date(epoch * 1000))
+            watchers.data[watcher_id].last_run = new Date(epoch * 1000)
+        }
 
         res.render('index.html', {
             users: users.data,
