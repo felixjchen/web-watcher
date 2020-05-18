12aff15ced459b4ec0a76dd02b5975516c7fe281
Felix Chen <felix.chen@ibm.com>
2020-05-17 22:11:52 -0400

    wait for webpage to load

--- src/screenshot/app.js
+++ src/screenshot/app.js
@@ -27,6 +27,7 @@ async function screenshot(url) {
     });
 
     await page.goto(url);
+    await new Promise(resolve => setTimeout(resolve, 1000));
     await page.screenshot({
         path: filePath,
         fullPage: true
