52b35c134d0567bd4f2ee9252fd976c39f39290b
Felix Chen <felixchen1998@gmail.com>
2020-06-18 23:31:03 -0400

    Update check.go

--- src/check/check.go
+++ src/check/check.go
@@ -9,7 +9,7 @@ import (
 )
 
 var now = time.Now().Unix()
-var threshold = 0.1
+var threshold = 0.0
 var cloud_object_storage_address = "http://0.0.0.0:8001"
 var compare_address = "http://0.0.0.0:8002"
 var screenshot_address = "http://0.0.0.0:8003"
