ce47378b614d5b66c7d920363c93c2608fde1593
Felix Chen <felixchen1998@gmail.com>
2020-08-22 23:44:48 -0400

    handler

--- src/frontend/src/components/login.js
+++ src/frontend/src/components/login.js
@@ -11,43 +11,39 @@ import {
 import "./login.css";
 import fractel from "./threetree_90.png";
 
-class Login extends React.Component {
-  render() {
-    return (
-      <div id="login">
-        <Header aria-label="header">
-          <HeaderName href="#" prefix="FC">
-            Watcher
-          </HeaderName>
-        </Header>
-        <Grid>
-          <Row>
-            <Column
-              id="leftBox"
-              sm={{ span: 4, offset: 0 }}
-              md={{ span: 4, offset: 1 }}
-              lg={{ span: 4, offset: 2 }}
-            >
-              <h1 className="bx--type-semibold">Sign in to your account</h1>
-              <TextInput id="email" labelText="Email" />
-              <TextInput.PasswordInput id="password" labelText="Password" />
-              <Button size="small" onClick={() => alert(1)}>
-                Log in
-              </Button>
-            </Column>
-            <Column
-              id="rightBox"
-              sm={{ span: 0 }}
-              md={{ span: 2 }}
-              lg={{ span: 4 }}
-            >
-              <img src={fractel} alt="fractel"></img>
-            </Column>
-          </Row>
-        </Grid>
-      </div>
-    );
-  }
-}
+const Login = (props) => (
+  <div id="login">
+    <Header aria-label="header">
+      <HeaderName href="#" prefix="FC">
+        Watcher
+      </HeaderName>
+    </Header>
+    <Grid>
+      <Row>
+        <Column
+          id="leftBox"
+          sm={{ span: 4, offset: 0 }}
+          md={{ span: 4, offset: 1 }}
+          lg={{ span: 4, offset: 2 }}
+        >
+          <h1 className="bx--type-semibold">Sign in to your account</h1>
+          <TextInput id="email" labelText="Email" />
+          <TextInput.PasswordInput id="password" labelText="Password" />
+          <Button size="small" onClick={props.handler}>
+            Log in
+          </Button>
+        </Column>
+        <Column
+          id="rightBox"
+          sm={{ span: 0 }}
+          md={{ span: 2 }}
+          lg={{ span: 4 }}
+        >
+          <img src={fractel} alt="fractel"></img>
+        </Column>
+      </Row>
+    </Grid>
+  </div>
+);
 
 export default Login;
--- src/frontend/src/index.js
+++ src/frontend/src/index.js
@@ -5,10 +5,20 @@ import Login from "./components/login";
 
 const production = typeof process.env.KUBERNETES_SERVICE_HOST !== "undefined";
 
-let configure_address = "http://0.0.0.0:8004";
+let gatewayAddress = "http://0.0.0.0:8008";
 if (production) {
   // configure_address = 'http://' + process.env.CONFIGURE_SERVICE_HOST + ':' + process.env.CONFIGURE_SERVICE_PORT
-  configure_address = "http://169.51.195.230:30001";
+  gatewayAddress = "http://169.51.195.230:30001";
 }
 
-render(<Login />, document.getElementById("root"));
+let loginButtonClickHandler = () => {
+  let email = document.getElementById("email").value;
+  let password = document.getElementById("password").value;
+
+  console.log(email, password);
+};
+
+render(
+  <Login handler={loginButtonClickHandler} />,
+  document.getElementById("root")
+);
