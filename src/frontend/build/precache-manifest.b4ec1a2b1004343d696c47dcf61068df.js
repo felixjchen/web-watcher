8cedec35c501dc2b36107b12e48bf24cf3db304a
Felix Chen <felixchen1998@gmail.com>
2020-09-19 20:38:51 -0400

    build

---
+++ src/frontend/build/precache-manifest.b4ec1a2b1004343d696c47dcf61068df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a90e0df4b13265aa5fbf",
+    "url": "./static/css/main.3a939fad.chunk.css"
+  },
+  {
+    "revision": "a90e0df4b13265aa5fbf",
+    "url": "./static/js/main.29c4d191.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "df62b696d5b9c61dcc1d",
+    "url": "./static/js/2.84f4c5bd.chunk.js"
+  },
+  {
+    "revision": "8428407b53e6b5c624eb12e8d5c6567d",
+    "url": "./static/media/threetree_90.8428407b.png"
+  },
+  {
+    "revision": "a472fccf56e3e7f831323c1a0151dc34",
+    "url": "./index.html"
+  }
+];
