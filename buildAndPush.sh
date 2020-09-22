fdf7c29c80019b0a5082d7e16ff0755d633614bc
Felix Chen <felixchen1998@gmail.com>
2020-09-22 19:49:42 -0400

    Update buildAndPush.sh

--- buildAndPush.sh
+++ buildAndPush.sh
@@ -3,4 +3,5 @@ cd src/$1
 docker build -t felixchen1998/web-watcher-$1:latest .
 docker push felixchen1998/web-watcher-$1:latest
 
-kubectl delete pods -l app=$1-app
+# kubectl delete pods -l app=$1-app
+kubectl rollout restart deployment $1-deployment
