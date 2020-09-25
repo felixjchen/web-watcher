847e9e97156d1218835fe243015acf3feb98f92a
Felix Chen <felixchen1998@gmail.com>
2020-09-25 17:34:04 -0400

    Update buildAndPushAll.sh

--- ops_src/buildAndPushAll.sh
+++ ops_src/buildAndPushAll.sh
@@ -1,9 +1,16 @@
+cd ../src
+
 for service in cloud_object_storage compare configure gateway notify screenshot token
-do 
-    cd ../src/$service
+do  
+    echo "=========================== $service ==========================="
+    cd $service
+
 
     docker build -t felixchen1998/web-watcher-$service:latest .
     docker push felixchen1998/web-watcher-$service:latest
 
     kubectl rollout restart deployment $service-deployment
+
+    cd ..
+    echo "=========================== /$service ==========================="
 done
