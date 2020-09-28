1da1659898221251e73d28f0037ea57e235261d8
Felix Chen <felixchen1998@gmail.com>
2020-09-27 22:30:41 -0400

    Update redeploy.sh

--- ops/redeploy.sh
+++ ops/redeploy.sh
@@ -17,14 +17,21 @@ if [ $# -eq 0 ];
             echo "--------------------------------- /$service ---------------------------------"
         done
 else
-    echo "--------------------------------- $1 ---------------------------------"
-    cd ../src/$1
+    cd ../src
 
-    docker build -t felixchen1998/web-watcher-$1:latest .
-    docker push felixchen1998/web-watcher-$1:latest
+    for service in "$@"
+    do
+        echo "--------------------------------- $service ---------------------------------"
+        cd $service
 
-    kubectl rollout restart deployment $1-deployment
-    echo "--------------------------------- /$1 ---------------------------------"
+        docker build -t felixchen1998/web-watcher-$service:latest .
+        docker push felixchen1998/web-watcher-$service:latest
+
+        kubectl rollout restart deployment $service-deployment
+
+        cd ..
+        echo "--------------------------------- /$service ---------------------------------"
+    done
 fi
             
 echo "================================= /done ================================="
